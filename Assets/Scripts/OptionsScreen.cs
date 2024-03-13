5b90547a6c57668b6255b1835b8681ff2e8861e2
Adrian <adrianmartinez1@csumb.edu>
2024-03-13 00:33:05 -0700

    Updated Level Select
    Updated Level Select

---
+++ Assets/Scripts/OptionsScreen.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsScreen : MonoBehaviour
+{
+    /* public Toggle fullscreenTog, vsyncTog;
+    string fullScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fullscreenTog.isOn = OptionsScreen.fullScreen;
+
+        if(QualitySettings.vSyncCount == 0)
+        {
+            vsyncTog.isOn = false;
+        } else
+        {
+            vsyncTog.isOn = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ApplyGraphics()
+    {
+        OptionsScreen.fullScreen = fullscreenTog.isOn;
+
+        if(vsyncTog.isOn)
+        {
+            QualitySetting.vSyncCount = 1;
+        }
+        else
+        {
+            QualitySettings.vSyncCount = 0;
+        };
+    } */
+}
